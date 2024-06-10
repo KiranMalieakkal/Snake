435345ea345b647e41245f7371b670f6eff1cb2f
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-06-10 22:26:01 +0200

    Adding some basic logic to the methods. Used the draw method to draw the apple and the snake body.The move method is also setup to move the snake.

--- src/main/java/com/kiran/GameFrame.java
+++ src/main/java/com/kiran/GameFrame.java
@@ -9,9 +9,9 @@ public class GameFrame extends JFrame {
     this.add(new GamePanel());
     this.setTitle("__SnakeGame__");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setResizable(false);
+    this.setResizable(true);
     this.pack();
-    this.setVisible(true);
+    this.setVisible(true);// this makes the frame visible when you create a new frame.
     this.setLocationRelativeTo(null);
     }
 }
--- src/main/java/com/kiran/GamePanel.java
+++ src/main/java/com/kiran/GamePanel.java
@@ -17,9 +17,9 @@ public class GamePanel extends JPanel implements ActionListener {
    final int[] y = new int[GAME_UNITS];
    int bodyParts = 6;
    int applesEaten = 0;
-   int AppleX;
-   int AppleY;
-   char direction = 'R'; // direction is used to set the direction of the snake, and we initilized it to R(for right) so that the sanke moves right when the game begins.
+   int appleX;
+   int appleY;
+   char direction = 'R'; // direction is used to set the direction of the snake, and we initilized it to R(for right) so that the snake moves right when the game begins.
    boolean running = false;
    Timer timer;
    Random random;
@@ -27,29 +27,74 @@ public class GamePanel extends JPanel implements ActionListener {
    public GamePanel(){
    random = new Random();
    this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
-   this.setBackground(Color.black);
+   this.setBackground(Color.red);
    this.setFocusable(true);
    this.addKeyListener(new MyKeyAdapter());
    startgame();
    }
 
    public void startgame(){
-
+   newApple();
+   running = true;
+   timer = new Timer(DELAY,this); // the timer function keeps triggering the actionlistener after every set DELAY
+   timer.start();
    }
 
+   @Override
    public void paintComponent(Graphics g){
-
+   super.paintComponent(g);
+   draw(g);
    }
 
    public void draw(Graphics g){
 
+      for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
+         g.setColor(Color.black);
+         g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+         g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+      }
+      g.setColor(Color.blue);
+      g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+
+      for (int i = 0; i <= bodyParts ; i++) {
+         if(i==0) {
+            g.setColor(Color.BLACK);
+            g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+         } else {
+            g.setColor(Color.white);
+            g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+
+         }
+
+      }
+
    }
 
    public void newApple(){
-
+   appleX = random.nextInt((int)SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
+   appleY = random.nextInt((int)SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
    }
 
    public void move(){
+      for (int i = bodyParts; i > 0 ; i--) {
+         x[i] = x[i-1];
+         y[i] = y[i-1];
+      }
+      switch (direction){
+         case 'U':
+            y[0] = y[0] - UNIT_SIZE;
+            break;
+         case 'D':
+            y[0] = y[0] + UNIT_SIZE;
+            break;
+         case 'L':
+            x[0] = x[0] - UNIT_SIZE;
+            break;
+         case 'R':
+            x[0] = x[0] + UNIT_SIZE;
+            break;
+
+      }
 
    }
    //this method checks if the snake eats the apple
@@ -65,6 +110,13 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+      if(running){
+         move();
+//         for (int i = 0; i <= bodyParts; i++) {
+//            System.out.println(x[i]);   // for visualising the movement of the snake
+//         }
+      }
+      repaint();// repaint will trigger the paintComponent to re render the painting
 
     }
 
